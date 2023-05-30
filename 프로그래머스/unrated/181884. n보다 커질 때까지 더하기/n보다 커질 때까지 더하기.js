582d33191c79c0caebeb4558d30613fd72ff109a
ZEON <119851517+zeon8080@users.noreply.github.com>
2023-05-30 13:20:44 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,13 +1,9 @@
 function solution(numbers, n) {
     let sum = 0
-let answer = 0;
-let arr = []
+
     for(let i = 0; i < numbers.length; i++){
   sum += numbers[i]
-  if( sum > n ){
-  arr.push(sum)
-   answer = [...arr].shift()
+ if(sum>n) break;
   }
-
-}        return answer
+        return sum;
 }
