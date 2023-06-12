79f74f2799f54c2355e6f5d3a0150f804a4adc9e
ZEON <119851517+zeon8080@users.noreply.github.com>
2023-06-12 16:24:08 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,11 +1,3 @@
 function solution(n, numlist) {
-    var arr = [];
-    
-for ( let i = 0; i < numlist.length; i++){
-  if(numlist[i]%n===0){
-    arr.push(numlist[i])
-  }
-}
-
-return arr
+  return numlist.filter(num=>num%n===0)
 }
