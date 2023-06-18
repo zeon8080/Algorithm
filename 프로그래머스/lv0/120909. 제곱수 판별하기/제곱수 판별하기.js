f1657a08dcae13bbd5d75332b84859c5fd4420d0
ZEON <119851517+zeon8080@users.noreply.github.com>
2023-06-18 17:11:29 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,5 +1,3 @@
 function solution(n) {
-  if(Number.isInteger(Math.sqrt(n))===true) result=1
-if(Number.isInteger(Math.sqrt(n))===false) result=2
-    return result
+  return Number.isInteger(Math.sqrt(n)) ? 1 : 2
 }
