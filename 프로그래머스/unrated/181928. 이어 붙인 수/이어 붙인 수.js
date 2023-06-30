29e50b4d63da5bbde46203baebdea95681df89f4
ZEON <119851517+zeon8080@users.noreply.github.com>
2023-06-30 13:37:30 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let arr1 =[]
+let arr2 =[]
+
+for(let i = 0; i < num_list.length; i++){
+  if(num_list[i] % 2 ===0) {
+    arr1.push(num_list[i])
+  } else {
+    arr2.push(num_list[i])
+  }
+}
+
+return Number(arr1.join('')) + Number(arr2.join(''))
+}
